6f3aebc903f33d9cf6003b00e3fb8f719de44a97
Humberto Cezar <skateforevis@hotmail.com>
2023-03-08 23:35:06 -0300

    import wallet

--- app/src/androidTest/java/com/example/byowsigner/CreateWalletTest.kt
+++ app/src/androidTest/java/com/example/byowsigner/CreateWalletTest.kt
@@ -20,4 +20,19 @@ class CreateWalletTest : BaseTest() {
         composeTestRule.onNodeWithText("Create")
             .performClick()
     }
+
+    @Test
+    fun shouldImportWallet() {
+        composeTestRule.onNodeWithContentDescription("Menu")
+            .performClick()
+        composeTestRule.onNodeWithText("New Wallet")
+            .performClick()
+        composeTestRule.onNodeWithContentDescription("Wallet Name")
+            .performTextInput("Test Wallet")
+        composeTestRule.onNodeWithContentDescription("Mnemonic Seed Words")
+            .performClick()
+            .performTextInput("about ".repeat(24).trimEnd())
+        composeTestRule.onNodeWithText("Create")
+            .performClick()
+    }
 }

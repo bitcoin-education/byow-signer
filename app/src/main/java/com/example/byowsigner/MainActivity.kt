c9d7196b75c46aaea2c82220a00b044526eb5d2a
Humberto Cezar <skateforevis@hotmail.com>
2023-03-11 19:53:43 -0300

    auth

--- app/src/main/java/com/example/byowsigner/MainActivity.kt
+++ app/src/main/java/com/example/byowsigner/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.byowsigner
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.provider.Settings
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.byowsigner.ui.screen.ByowHome
+import androidx.fragment.app.FragmentActivity
+import com.example.byowsigner.ui.screen.LoginScreen
 import com.example.byowsigner.ui.theme.BYOWSignerTheme
 import com.example.byowsigner.ui.viewmodels.CreateWalletViewModel
 import com.example.byowsigner.ui.viewmodels.ExportWatchOnlyWalletViewModel
@@ -16,11 +21,12 @@ import com.example.byowsigner.ui.viewmodels.SignTransactionViewModel
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.Security
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Security.removeProvider("BC")
         Security.addProvider(BouncyCastleProvider())
+        setupAuthentication()
 
         val createWalletViewModel: CreateWalletViewModel by viewModels { CreateWalletViewModel.Factory }
         val signTransactionViewModel: SignTransactionViewModel by viewModels { SignTransactionViewModel.Factory }
@@ -32,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ByowHome(
+                    LoginScreen(
                         createWalletViewModel = createWalletViewModel,
                         signTransactionViewModel = signTransactionViewModel,
                         exportWatchOnlyWalletViewModel = exportWatchOnlyWalletViewModel
@@ -41,4 +47,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun setupAuthentication() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                    putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED, DEVICE_CREDENTIAL)
+                }
+                startActivityForResult(enrollIntent, 1234)
+            }
+            else -> {}
+        }
+    }
+
 }
